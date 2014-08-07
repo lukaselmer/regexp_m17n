4deb0308047084e2bc1f6bb3fffbed92703f20a8
Lukas Elmer <lukas.elmer@gmail.com>
2014-08-07 20:40:41 +0200

    fix test

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,6 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+    str = str.force_encoding(Encoding::UTF_8) if str.encoding.dummy?
+    str.encode(Encoding::UTF_8) =~ /^.+$/
   end
 end
