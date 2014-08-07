07bd6455474021eb9eed4c2753ef064546c833cc
Lukas Elmer <lukas.elmer@gmail.com>
2014-08-07 20:37:24 +0200

    Test dummy and normal encodings separately

--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -3,9 +3,15 @@ require 'minitest/autorun'
 require_relative '../lib/regexp_m17n'
 
 class RegexpTest < MiniTest::Unit::TestCase
-  def test_non_empty_string
-    Encoding.list.each do |enc|
+  def test_non_empty_string_without_dummy_encodings
+    Encoding.list.reject(&:dummy?).each do |enc|
       assert(RegexpM17N.non_empty?('.'.encode(enc)))
+    end
+  end
+
+  def test_non_empty_string_with_dummy_encodings
+    Encoding.list.select(&:dummy?).each do |enc|
+      assert(RegexpM17N.non_empty?('.'.force_encoding(enc)))
     end
   end
 end
